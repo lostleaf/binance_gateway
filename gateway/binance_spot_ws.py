4ec6aa929580699c2a3703429a6a122d3797dd56
Qinglin Li <qinglin@limao.tech>
2021-11-26 15:18:09 +0800

    Subscribe 1min candle

--- gateway/binance_spot_ws.py
+++ gateway/binance_spot_ws.py
@@ -1,14 +1,18 @@
-from .websocket_client import WebsocketClient
 import logging
+
+import pandas as pd
+
+from .constant import CandleData
+
+from .websocket_client import WebsocketClient
+
 
 class BinanceSpotWs(WebsocketClient):
     """币安现货行情Websocket API"""
-
     def __init__(self) -> None:
         """构造函数"""
         super().__init__()
         self.reqid = 0
-
 
     def connect(self):
         """连接Websocket行情频道"""
@@ -22,12 +26,9 @@ class BinanceSpotWs(WebsocketClient):
         logging.info("行情Websocket API连接成功")
 
         # 重新订阅行情
-        req: dict = {
-            "method": "SUBSCRIBE",
-            "params": ['btcusdt@kline_1m'],
-            "id": self.reqid
-        }
+        req: dict = {"method": "SUBSCRIBE", "params": ['btcusdt@kline_1m'], "id": self.reqid}
         self.send_packet(req)
+        self.reqid += 1
 
     def on_packet(self, packet: dict) -> None:
         """推送数据回报"""
@@ -35,4 +36,20 @@ class BinanceSpotWs(WebsocketClient):
 
         if not stream:
             return
-        print(packet)
+
+        if 'kline' in stream:
+            d = packet['data']['k']
+            if not d['x']: # candle not closed
+                return
+            candle = CandleData(candle_begin_time=pd.to_datetime(int(d['t']), unit='ms', utc=True),
+                                caldne_end_time=pd.to_datetime(int(d['T']), unit='ms', utc=True),
+                                open=float(d['o']),
+                                high=float(d['h']),
+                                low=float(d['l']),
+                                close=float(d['c']),
+                                volume=float(d['v']),
+                                turnover=float(d['q']),
+                                num_trades=int(d['n']),
+                                buy_vol=float(d['V']),
+                                buy_turnover=float(d['Q']))
+            print(candle)
